0cf9d19a899b2f3a59253a5441ed81d621bcf87b
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-09-26 18:36:34 +0530

    created Menu section

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
       <Cocktails/>
       <About/>
       <Art/>
+      <Menu/>
     </main>
   )
 }

485a5b6f54bbdfea77ee3ecab4288582ab9eaad6
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-09-30 01:06:24 +0530

    updated

--- website/src/components/Art.jsx
+++ website/src/components/Art.jsx
@@ -41,7 +41,7 @@ const Art = () => {
 		 
 		 <div className="cocktail-img">
 			<img
-				src="/images/under-img.jpg"
+				src="/images/waiter.jpg"
 				alt="cocktail"
 				className="abs-center masked-img size-full object-contain"
 			/>
